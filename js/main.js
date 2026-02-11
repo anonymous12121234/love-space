77ec941568b4189bada4990a5288e5c3003bde7a
xiaolinxiaoyu <1299228277@qq.com>
2026-02-12 05:47:45 +0800

    minor

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ function formatDate(date) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  return `${year}年${month}月${day}日`;
+  return `${year}-${month}-${day}`;
 }
 
 function setTextById(id, text) {
